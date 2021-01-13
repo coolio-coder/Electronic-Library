263398611bb457e881bb2a2c6f8abbcb1532a542
Jeffrey Ou <lojeffreyou@gmail.com>
2021-01-12 20:37:23 -0500

    add triangle feature for book frame

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,10 @@
 var myLibrary = [];
 
-const book1 = new books('Harry Potter', 'Jk Rowling', 636, 'read');
-const book2 = new books('A Song of Ice and Fire', 'George R. R. Martin', 694, 'not read');
-const book3 = new books('Eragon', 'Christopher Paolini', 509, 'read');
+const book1 = new Books('Harry Potter', 'Jk Rowling', 636, 'read');
+const book2 = new Books('A Song of Ice and Fire', 'George R. R. Martin', 694, 'not read');
+const book3 = new Books('Eragon', 'Christopher Paolini', 509, 'read');
 
-function books (title, author, page, read) {
+function Books (title, author, page, read) {
   this.title = title;
   this.author = author; 
   this.page = page;
@@ -23,21 +23,36 @@ function books (title, author, page, read) {
   }
 }
 
-book1.addBook();
-book2.addBook();
-book3.addBook();
-
-
-
+var triangleInfo = []
+var trianglePosition = new Object();
 
 function addTriangleUp() {
   const div = document.createElement('div');
-  div.className = 'triangle-up';
-  document.getElementById('hey').appendChild(div);
+  div.className = 'triangle up';
+  document.getElementById('unsorted').appendChild(div);
+}
+
+function addTriangle () {
+  //Use triangle array to detect the triangle position
+  if (triangleInfo.length === 0) {
+    addTriangleUp();
+    //Set the position of the triangle to be true then push onto the array
+    trianglePosition.isUp = true;
+    triangleInfo.push(trianglePosition);
+  } else if (triangleInfo[0]['isUp'] === true) {
+    addTriangleDown();
+    //Set the position of the triangle to be false
+    trianglePosition.isUp = false;
+  } else {
+    addTriangleUp();
+    trianglePosition.isUp = true;
+  }
+  //Add triangle up or down
+  //Update the triangle array
 }
 
 function addTriangleDown() {
   const div = document.createElement('div');
-  div.className = 'triangle-down';
-  document.getElementById('hey').appendChild(div);
+  div.className = 'triangle down';
+  document.getElementById('unsorted').appendChild(div);
 }
